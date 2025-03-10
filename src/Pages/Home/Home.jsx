027e4bd6b0cd15869d74cc4fae55900e5f4c96e4
sagarrb1805 <sagarrb1805@gmail.com>
2025-03-10 15:15:14 +0530

    home page sample

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
-import HomeImage from '../../Images/home_image.png';
+import SecondImage from '../../Images/home_image.png';
+import FirstImage from '../../Images/jobs-animation-image.jpg'
 import styles from './Home.module.css';
 import Carousel from 'react-bootstrap/Carousel';
 // import ExampleCarouselImage from 'components/ExampleCarouselImage';
@@ -9,21 +10,21 @@ function Home() {
   return (
     <Carousel>
       <Carousel.Item interval={2000}>
-      <Image className={styles['full-width-image']} src={HomeImage} alt='Home image' />
+      <Image className={styles['full-width-image']} src={FirstImage} alt='Home image' />
         <Carousel.Caption>
           <h3 className={styles['carousel-header']}>Unleashing Creativity</h3>
           <p className={styles['carousel-text']}>Where imagination meets animation—crafting worlds beyond reality.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={2000}> 
-      <Image className={styles['full-width-image']} src={HomeImage} alt='Home image' />
+      <Image className={styles['full-width-image']} src={SecondImage} alt='Home image' />
         <Carousel.Caption>
           <h3 className={styles['carousel-header']}>Innovative Animation</h3>
           <p className={styles['carousel-text']}>Pushing the boundaries of technology to bring your stories to life.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={2000}>
-      <Image className={styles['full-width-image']} src={HomeImage} alt='Home image' />
+      <Image className={styles['full-width-image']} src={FirstImage} alt='Home image' />
         <Carousel.Caption>
           <h3 className={styles['carousel-header']}>Mastering the Art of Storytelling</h3>
           <p className={styles['carousel-text']}>
